f42595738a3c86dc6c08863a2070840c44c42faa
BackToTheCrm <clem.marty@gmail.com>
2015-11-10 16:43:28 +0100

    Removing JSON2 and jQueryUI libraries

---
+++ BackToTheCrm.Signature/BackToTheCrm.Signature/scripts/lang/signature.en.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="library/jquery.js" />
+
+(function ($) {
+    $.extend(true, window, {
+        "BackToTheCrm": {
+            "Signature": {
+                "messages": {
+                    en: {
+                        title: "Signature",
+                        description: "Sign above",
+                        clear: "Clear",
+                        save: "Save",
+                        saveSignatureAlert: "Please sign before clicking Save.",
+                        saveCreateAlert: "The record must be created before saving the signature.",
+                        paramError: "The parameters contains error. Please contact your administrator.",
+                        saveError: "An error has occured while saving."
+                    }
+                }
+            }
+        }
+    });
+})(jQuery);
